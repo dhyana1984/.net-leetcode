9ed390b5c07379869dbd0a34ee0b8a217acf94d1
dhyana1984 <xiongyi_1984@foxmail.com>
2019-04-25 23:42:04 +0800

    向数组插入元素并返回索引

---
+++ LeetCode/LeetCode/Easy/SearchInsert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+   public class SolutionSearchInsert
+    {
+      
+        public int SearchInsert(int[] nums, int target)
+        {
+           
+        }
+        
+    }
+}
