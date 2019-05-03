218f4f32312241692e8d7d98964d335e7f2c4f1a
dhyana1984 <xiongyi_1984@foxmail.com>
2019-05-04 01:52:00 +0800

    合并两个有序数组

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -13,13 +13,13 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            ListNode head = new ListNode(1);
-            head.next = new ListNode(1);
-            head.next.next = new ListNode(2);
-            head.next.next.next = new ListNode(3);
-            head.next.next.next.next = new ListNode(3);
-            DeleteListDuplicates solution = new DeleteListDuplicates();
-            solution.Solution(head);
+            //ListNode head = new ListNode(1);
+            //head.next = new ListNode(1);
+            //head.next.next = new ListNode(2);
+            //head.next.next.next = new ListNode(3);
+            //head.next.next.next.next = new ListNode(3);
+            MergeSortedArray solution = new MergeSortedArray();
+            solution.Solution(new int[] { 4, 5, 6, 0, 0, 0 },3,new int[] { 1, 2, 3 },3);
         }
     }
 }
