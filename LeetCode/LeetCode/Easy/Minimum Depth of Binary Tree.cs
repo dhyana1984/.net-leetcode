cc7753488d5863222c40e23803a26d03fdb29e6a
dhyana1984 <xiongyi_1984@foxmail.com>
2019-05-11 01:12:56 +0800

    二叉树最小深度解答

---
+++ LeetCode/LeetCode/Easy/Minimum Depth of Binary Tree.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCode.Easy.IsSameTree;
+
+namespace LeetCode.Easy
+{
+    public class Minimum_Depth_of_Binary_Tree
+    {
+        /*
+         * 给定一个二叉树，找出其最小深度。
+
+        最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+        说明: 叶子节点是指没有子节点的节点。
+
+        示例:
+
+        给定二叉树 [3,9,20,null,null,15,7],
+
+        3
+        / \
+        9  20
+        /  \
+        15   7
+        返回它的最小深度  2.
+         */
+        public int Solution(TreeNode root)
+            {
+                //遍历解答 
+
+                //if (root == null)
+                //    return 0;
+                //Queue<TreeNode> queue = new Queue<TreeNode>();
+                //queue.Enqueue(root);
+                //int count;
+                //int level = 1;
+                //while (queue.Any())
+                //{
+                //    count = queue.Count;
+                //    for (int i = 0; i < count; i++)
+                //    {
+                //        TreeNode node = queue.Dequeue();
+                //        if (node.left != null)
+                //            queue.Enqueue(node.left);
+                //        if (node.right != null)
+                //            queue.Enqueue(node.right);
+                //        if (node.right == null && node.left == null)
+                //            return level;
+
+                //    }
+                //    level++;
+                //}
+
+                //return 1;
+
+                //递归解答 
+                if (root == null)
+                    return 0;
+        
+                else
+                {
+                    
+                         
+                    int left = Solution(root.left);
+                    int right = Solution(root.right);
+                    if (left == 0)
+                        return right + 1;
+                    else if(right ==0)
+                        return left + 1;
+                    else
+                        return Math.Min(left, right) + 1;
+                 
+                }
+
+
+
+
+            }
+
+
+
+        
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -15,18 +15,18 @@ namespace LeetCode
         static void Main(string[] args)
         {
             TreeNode t1 = new TreeNode(1);
-            t1.left = new TreeNode(1); ;
+            t1.left = null;
             t1.right = new TreeNode(2);
 
             //t1.left.left = new TreeNode(3);
             //t1.left.right = new TreeNode(3);
-            t1.right.left = null;
-            t1.right.right = new TreeNode(3);
-            t1.right.right.left = new TreeNode(5);
+            t1.right.left = new TreeNode(3);
+            t1.right.right = new TreeNode(4);
+            t1.right.left.left = new TreeNode(5);
             //t1.left.left.left = new TreeNode(4);
             //t1.left.left.right = new TreeNode(4);
 
-            SolutionIsBalanced solution = new SolutionIsBalanced();
+            Minimum_Depth_of_Binary_Tree solution = new Minimum_Depth_of_Binary_Tree();
            var aaaa= solution.Solution(t1);
         }
     }
