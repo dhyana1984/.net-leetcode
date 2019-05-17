681425daa28c47f322b94e6b77e62039a8f54897
dhyana1984 <xiongyi_1984@foxmail.com>
2019-05-17 23:52:09 +0800

    计算给定二叉树的所有左叶子之和

---
+++ LeetCode/LeetCode/Easy/SumOfLeftLeaves.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class SumOfLeftLeaves
+    {
+
+        //int sum = 0;
+        public int Solution(TreeNode root)
+        {
+            //if (root == null)
+            //    return 0;
+            //if (root.left != null)
+            //{
+            //Solution(root.left);
+            //    if (root.left.left == null && root.left.right == null)
+            //        sum += root.left.val;
+
+            //}
+            //if (root.right != null)
+            //{
+            //    Solution(root.right);
+            //}
+
+            //return sum;
+
+            if (root == null)
+                return 0;
+            int sum = 0;
+            if (root.left != null)
+            {
+                if (root.left.left == null && root.left.right == null)
+                    sum = root.left.val;
+            }
+            sum += Solution(root.left) + Solution(root.right);
+            return sum;
+        }
+    }
+
+}
+
