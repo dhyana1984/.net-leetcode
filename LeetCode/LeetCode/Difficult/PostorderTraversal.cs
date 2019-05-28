2e36de89cd84a6a2f618fa081d5c4e523a4e9ea3
dhyana1984 <xiongyi_1984@foxmail.com>
2019-05-29 00:16:23 +0800

    树的后续遍历

---
+++ LeetCode/LeetCode/Difficult/PostorderTraversal.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Difficult
+{
+    /*
+     * 给定一个二叉树，返回它的 后序 遍历。迭代方式
+     */
+    public class PostorderTraversal
+    {
+        public IList<int> Solution(TreeNode root)
+        {
+            IList<int> resultList = new List<int>();
+            if (root != null)
+            {
+                Stack<TreeNode> stack = new Stack<TreeNode>();
+                stack.Push(root);
+                while (stack.Any())
+                {
+                    TreeNode node = stack.Pop();
+                    resultList.Insert(0, node.val);
+                    if (node.left != null)
+                        stack.Push(node.left);
+                    if (node.right != null)
+                        stack.Push(node.right);
+                }
+            }
+
+            return resultList;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Easy/N-aryTreePostorderTraversal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 给定一个 N 叉树，返回其节点值的后序遍历。
+     */
+    public class N_aryTreePostorderTraversal
+    {
+        public IList<int> Postorder(Node root)
+        {
+            IList<int> resultList = new List<int>();
+            if (root != null)
+            {
+                Recursive(root.children, resultList);
+                resultList.Add(root.val);
+            }
+            return resultList;
+        }
+
+        private void Recursive(IList<Node> children, IList<int> resultList)
+        {
+            if (children == null) return;
+            for (int i = 0; i < children.Count; i++)
+            {
+                Recursive(children[i].children, resultList);
+                resultList.Add(children[i].val);
+            }
+
+        }
+    }
+}
