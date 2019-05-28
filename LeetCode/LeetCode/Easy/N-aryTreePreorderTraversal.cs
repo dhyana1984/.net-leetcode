7e51754559e4ce2a4db1dada6bbba9eca54f97a9
dhyana1984 <xiongyi_1984@foxmail.com>
2019-05-28 23:27:50 +0800

    给定一个 N 叉树，返回其节点值的前序遍历。

---
+++ LeetCode/LeetCode/Easy/N-aryTreePreorderTraversal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /*
+     * 给定一个 N 叉树，返回其节点值的前序遍历。
+     */
+    public class N_aryTreePreorderTraversal
+    {
+        public IList<int> Solution(Node root)
+        {
+
+            IList<int> resultList = new List<int>();
+            PreOrder(root, resultList);
+            return resultList;
+
+        }
+
+        private void PreOrder(Node root, IList<int> resultList)
+        {
+            if (root == null) return;
+            resultList.Add(root.val);
+
+            if (root.children != null)
+                for (int i = 0; i < root.children.Count; i++)
+                {
+                    PreOrder(root.children[i], resultList);
+                }
+
+
+        }
+    }
+}
