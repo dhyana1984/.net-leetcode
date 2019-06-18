29cb62440f445b114580e7d5dc255bf75eef3b9c
dhyana1984 <xiongyi_1984@foxmail.com>
2019-06-19 02:02:02 +0800

    二叉搜索树中的中序后继1和2

---
+++ LeetCode/LeetCode/Medium/Tree/InorderSuccessor1.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Medium.Tree
+{
+    /*
+     给你一个二叉搜索树和其中的某一个结点，请你找出该结点在树中顺序后继的节点。
+
+结点 p 的后继是值比 p.val 大的结点中键值最小的结点。
+         */
+
+    public class InorderSuccessor1
+    {
+        TreeNode result = null;
+        bool flag = false;
+        public TreeNode Solution(TreeNode root, TreeNode p)
+        {
+            InOrder(root, p);
+            return result;
+        }
+
+        private void InOrder(TreeNode node, TreeNode target)
+        {
+            if (node == null)
+                return;
+            InOrder(node.left, target);
+            if (flag && result == null)
+                result = node;
+            if (node == target)
+                flag = true;
+
+            InOrder(node.right, target);
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Medium/Tree/InorderSuccessor2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Medium.Tree
+{
+    /*
+     * 给定一棵二叉搜索树和其中的一个节点，找到该节点在树中的中序后继。
+
+        一个结点 p 的中序后继是键值比 p.val大所有的结点中键值最小的那个。
+
+        你可以直接访问结点，但无法直接访问树。每个节点都会有其父节点的引用。
+
+
+     */
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    public class InorderSuccessor2
+    {
+        Node result = null;
+        bool flag = false;
+        public Node Solution(Node x)
+        {
+            if (x == null) return null;
+            Node root = x;
+            while (root.parent != null)
+            {
+                root = root.parent;
+            }
+            InOrder(root, x);
+            return result;
+        }
+
+
+        private void InOrder(Node node, Node target)
+        {
+            if (node == null)
+                return;
+            InOrder(node.left, target);
+            if (flag && result == null)
+                result = node;
+            if (node == target)
+                flag = true;
+
+            InOrder(node.right, target);
+        }
+    }
+}
