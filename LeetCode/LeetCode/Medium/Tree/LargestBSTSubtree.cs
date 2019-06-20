bddebcbca6a3845f823a6561aaf46e78fe51a67d
dhyana1984 <xiongyi_1984@foxmail.com>
2019-06-21 00:05:57 +0800

    给定一个二叉树，找到其中最大的二叉搜索树（BST）子树

---
+++ LeetCode/LeetCode/Medium/Tree/LargestBSTSubtree.cs
@@ -0,0 +1,83 @@
+﻿using LeetCode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Medium.Tree
+{
+    /*
+     * 给定一个二叉树，找到其中最大的二叉搜索树（BST）子树，其中最大指的是子树节点数最多的。
+
+        注意:
+        子树必须包含其所有后代。
+
+        示例:
+
+        输入: [10,5,15,1,8,null,7]
+
+           10 
+           / \ 
+          5  15 
+         / \   \ 
+        1   8   7
+
+        输出: 3
+        解释: 高亮部分为最大的 BST 子树。
+             返回值 3 在这个样例中为子树大小。
+
+
+     */
+
+    public class LargestBSTSubtree
+    {
+        int result = 0;
+        int count = 0;
+        List<int> list = new List<int>();
+        public int Solution(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            FindMaxBST(root);
+            return result;
+        }
+
+        private bool FindMaxBST(TreeNode root)
+        {
+
+            bool lbst = true;
+            bool rbst = true;
+            bool bst = true;
+            int l = list.Count;
+            int r = list.Count;
+            if (root.left != null)
+            {
+                lbst = FindMaxBST(root.left);
+                if (!lbst || root.val <= list.Last())
+                {
+                    bst = false;
+                }
+
+            }
+            list.Add(root.val);
+            r = list.Count;
+            if (root.right != null)
+            {
+                rbst = FindMaxBST(root.right);
+                if (!rbst || root.val >= list[r])
+                {
+                    bst = false;
+                }
+            }
+            r = list.Count;
+            if (bst)
+            {
+                result = Math.Max(result, r - l);
+            }
+
+            return bst;
+
+        }
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -17,14 +17,14 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            TreeNode t1 = new TreeNode(5);
-            t1.left = new TreeNode(2); 
-            t1.right = new TreeNode(1);
-            t1.left.left = new TreeNode(3);
+            TreeNode t1 = new TreeNode(10);
+            t1.left = new TreeNode(5); 
+            t1.right = new TreeNode(15);
+            t1.left.left = new TreeNode(1);
             
-            t1.left.right = new TreeNode(6);
+            t1.left.right = new TreeNode(8);
             //t1.right.left = new TreeNode(0);
-            //t1.right.right = new TreeNode(6);
+            t1.right.right = new TreeNode(7);
             //t1.left.right.left = new TreeNode(7);
             //t1.left.right.right = new TreeNode(4);
             //t1.left.left.left = new TreeNode(0);
@@ -59,8 +59,8 @@ namespace LeetCode
             //int[] a2 = new int[] { 9, 15, 7, 20, 3 };
             //var aaaa = solution.BuildTree(a1,a2);
             var aaa = new int[] { 1,3,2};
-            VerifyPreorderBST solution = new VerifyPreorderBST();
-            var result = solution.Solution(aaa);
+            LargestBSTSubtree solution = new LargestBSTSubtree();
+            var result = solution.Solution(t1);
 
             //GetMatchAmountList solution = new GetMatchAmountList();
             //List<int> list1 = new List<int> { 500,600,200,300,550 };
