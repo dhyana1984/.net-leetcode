1c441da2496f9a68105b4010c39dfaacdf314e2d
dhyana1984 <xiongyi_1984@foxmail.com>
2019-07-02 00:33:41 +0800

    给定一棵二叉树，返回所有重复的子树

---
+++ LeetCode/LeetCode/Medium/Tree/FindDuplicateSubtrees.cs
@@ -0,0 +1,71 @@
+﻿using LeetCode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Medium.Tree
+{
+    /*
+     * 给定一棵二叉树，返回所有重复的子树。对于同一类的重复子树，你只需要返回其中任意一棵的根结点即可。
+
+        两棵树重复是指它们具有相同的结构以及相同的结点值。
+
+        示例 1：
+
+                1
+               / \
+              2   3
+             /   / \
+            4   2   4
+               /
+              4
+        下面是两个重复的子树：
+
+              2
+             /
+            4
+        和
+
+            4
+        因此，你需要以列表的形式返回上述重复子树的根结点。
+
+
+     */
+
+    public class FindDuplicateSubtrees
+    {
+        Dictionary<string, IList<TreeNode>> dict = new Dictionary<string, IList<TreeNode>>();
+        IList<TreeNode> list = new List<TreeNode>();
+        public IList<TreeNode> Solution(TreeNode root)
+        {
+            PostOrder(root);
+
+            return list;
+
+
+        }
+
+        private string PostOrder(TreeNode node)
+        {
+            if (node == null)
+            {
+                return "N";
+            }
+            string left = PostOrder(node.left);
+            string right = PostOrder(node.right);
+            string s = node.val.ToString() + left + right;
+            if (!dict.ContainsKey(s))
+                dict[s] = new List<TreeNode> { node };
+            else
+            {
+                dict[s].Add(node);
+                if (dict[s].Count > 1 && !list.Contains(dict[s].First()))
+                    list.Add(dict[s].First());
+            }
+
+            return s;
+        }
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -17,17 +17,19 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            TreeNode t1 = new TreeNode(3);
+            TreeNode t1 = new TreeNode(1);
             t1.left = new TreeNode(2);
-            t1.right = new TreeNode(4);
-            //t1.left.left = new TreeNode(4);
+            t1.right = new TreeNode(3);
+            t1.left.left = new TreeNode(4);
 
             //t1.left.right = new TreeNode(3);
-            t1.right.left = new TreeNode(1);
-            //t1.right.right = new TreeNode(5);
-            //t1.left.right.left = new TreeNode(7);
-            //t1.left.right.right = new TreeNode(3);
+            t1.right.left = new TreeNode(2);
+            t1.right.right = new TreeNode(4);
+            //t1.left.left.left = new TreeNode(7);
+            //t1.left.left.right = new TreeNode(3);
+            //t1.left.right.right = new TreeNode(4);
             //t1.left.left.left = new TreeNode(0);
+            t1.right.left.left = new TreeNode(4);
             ////t1.left.left.right = new TreeNode(1);
             ////t1.left.left.left.left = new TreeNode(4);
             ////t1.left.left.left.left.left = new TreeNode(8);
@@ -59,7 +61,7 @@ namespace LeetCode
             //int[] a2 = new int[] { 9, 15, 7, 20, 3 };
             //var aaaa = solution.BuildTree(a1,a2);
             var aaa = new int[] { 1,3,2};
-            LongestConsecutiveII solution = new LongestConsecutiveII();
+            FindDuplicateSubtrees solution = new FindDuplicateSubtrees();
             var result = solution.Solution(t1);
 
             //GetMatchAmountList solution = new GetMatchAmountList();
