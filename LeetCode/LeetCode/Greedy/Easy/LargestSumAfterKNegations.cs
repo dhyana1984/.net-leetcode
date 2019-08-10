b5dd32fd979c416e184b214ef0cc5efe73a0e36b
dhyana1984 <xiongyi_1984@foxmail.com>
2019-08-10 23:29:37 +0800

    K 次取反后最大化的数组和

---
+++ LeetCode/LeetCode/Greedy/Easy/LargestSumAfterKNegations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Greedy.Easy
+{
+
+   public class LargestSumAfterKNegations
+    {
+        public int Solution(int[] A, int K)
+        {
+            var i = 0;
+            while (i < K)
+            {
+                i++;
+                var index = Array.IndexOf(A,A.Min());
+                A[index] *= -1;
+            }
+            return A.Sum();
+        }
+    }
+}
