85ecab002e86336382a93fcb575011da876e90ce
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-08-11 01:41:58 +0800

    remove

---
+++ LeetCodeDotNetCore/LeetCodeDotNetCore/DP/Medium/Coins.js
@@ -0,0 +1,41 @@
+﻿/**
+ * 
+ *
+    硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，编写代码计算n分有几种表示法。(结果可能会很大，你需要将结果模上1000000007)
+
+    示例1:
+
+     输入: n = 5
+     输出：2
+     解释: 有两种方式可以凑成总金额:
+    5=5
+    5=1+1+1+1+1
+    示例2:
+
+     输入: n = 10
+     输出：4
+     解释: 有四种方式可以凑成总金额:
+    10=10
+    10=5+5
+    10=5+1+1+1+1+1
+    10=1+1+1+1+1+1+1+1+1+1
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var waysToChange = function (n) {
+    const dp = new Array(n + 1)
+    const coins = [1, 5, 10, 25]
+    for (let i = 0; i < n + 1; i++) dp[i] = 0
+    dp[0] = 1
+    for (c in coins) {
+        const coin = coins[c]
+        for (let i = coin; i <= n; i++) {
+            dp[i] = dp[i] + dp[i - coin]
+        }
+    }
+    return dp[n] % 1000000007
+
+};
