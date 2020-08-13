d9b4eda8223f8ad8a90db27a2cded83a056d8976
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-08-14 00:03:10 +0800

    简单题

---
+++ LeetCodeDotNetCore/LeetCodeDotNetCore/ReverseString.js
@@ -0,0 +1,14 @@
+﻿/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+    if (!s.length) return []
+    let i = 0, j = s.length - 1
+    while (i <= j) {
+        [s[i], s[j]] = [s[j], s[i]]
+        i++
+        j--
+    }
+    return s
+};
