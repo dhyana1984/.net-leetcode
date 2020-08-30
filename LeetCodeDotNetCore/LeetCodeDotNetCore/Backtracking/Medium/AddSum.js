34306e3cf5c811a738f99fb6fc154ae7a1851cce
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-08-30 14:19:17 +0800

    刷题

---
+++ LeetCodeDotNetCore/LeetCodeDotNetCore/Backtracking/Medium/AddSum.js
@@ -0,0 +1,13 @@
+﻿/*
+ * 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+
+
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumNums = function (n) {
+    return n && sumNums(n - 1) + n
+};
